71c5f6d24f0dbe6c247075305bdfb87e45aef04c
Renan Dorneles Schuquel <rdorneles64@gmail.com>
2024-02-04 21:52:11 -0300

    Refactor on Board struct.

--- src/board.rs
+++ src/board.rs
@@ -3,9 +3,8 @@ use dyn_fmt::AsStrFormatExt;
 #[derive(Debug)]
 pub struct Board {
     pub board: Vec<char>,
-    pub parsed_logic_board: Vec<Vec<char>>,
+    pub parsed_logic_board: Option<Vec<Vec<char>>>,
     pub visual_board: String,
-    pub history: Vec<String>,
     board_template: String,
 }
 
@@ -19,9 +18,8 @@ impl Board {
             | {} | {} | {} |
         ".to_string();
 
-        let mut board = Board { board: vec![' '; 9], parsed_logic_board: vec![vec![]], visual_board: String::new(), history: vec![], board_template };
+        let mut board = Board { board: ('1'..='9').collect(), parsed_logic_board: None, visual_board: String::new(), board_template };
         
-        board.parsed_logic_board = board.parse_logic_board();
         board.initialize_visual_board();
 
         board
@@ -45,49 +43,43 @@ impl Board {
     }
 
     pub fn play_already_throwed(&mut self, user_play: u8) -> bool {
-        let user_play_index = (user_play - 1) as usize;
-        let play_at_index = self.board.get(user_play_index).unwrap().clone();
+        let play_at_index = self.board.get((user_play - 1) as usize).unwrap().clone();
         
         play_at_index == '⬤' || play_at_index == '✖'
     }
 
     fn update_logic_board(&mut self, user_play: u8, new_symbol: char) {
-        let logic_board_reference = self.board.get_mut((user_play - 1) as usize).unwrap();
-        *logic_board_reference = new_symbol;
+        let position = self.board.get_mut((user_play - 1) as usize).unwrap();
+        *position = new_symbol;
+
+        self.update_parsed_logic_board()
     }
 
     fn update_visual_board(&mut self) {
-        self.add_visual_board_history();
-
         let board = self.board_template.format(&self.board);
         self.visual_board = board;
     }
 
-    // TODO: Needed?
-    fn add_visual_board_history(&mut self) {
-        self.history.push(self.visual_board.clone());
-    }
-
     pub fn has_winner(&self) -> bool {
-        let parsed_logic_board = self.parse_logic_board();
-        if self.has_line_winner(&parsed_logic_board) == true {
+        if self.has_line_winner() == true {
             return true;
         }
 
-        if self.has_column_winner(&parsed_logic_board) == true {
+        if self.has_column_winner() == true {
             return true;
         }
 
-        if self.has_diagonal_winner(&parsed_logic_board) == true {
+        if self.has_diagonal_winner() == true {
             return true;
         }
 
         false
     }
 
-    fn has_line_winner(&self, parsed_logic_board: &Vec<Vec<char>>) -> bool {
-        for line in parsed_logic_board {
-            if self.is_vector_winner(line) == true {
+    fn has_line_winner(&self) -> bool {
+        println!("HAS LINE WINNER?");
+        for line in self.parsed_logic_board.as_ref().unwrap() {
+            if self.is_vector_winner(&line) == true {
                 return true;
             }
         }
@@ -95,10 +87,10 @@ impl Board {
         false
     }
 
-    fn has_column_winner(&self, parsed_logic_board: &Vec<Vec<char>>) -> bool {
+    fn has_column_winner(&self) -> bool {
         for i in 0..3 {
             let mut column_in_line: Vec<char> = vec![];
-            for line in parsed_logic_board.iter() {
+            for line in self.parsed_logic_board.as_ref().unwrap().iter() {
                 column_in_line.push(line.get(i).unwrap().clone());
             }
 
@@ -110,11 +102,11 @@ impl Board {
         false
     }
 
-    fn has_diagonal_winner(&self, parsed_logic_board: &Vec<Vec<char>>) -> bool {
+    fn has_diagonal_winner(&self) -> bool {
         let mut diagonal_in_line: Vec<char> = vec![];
         let mut incremented_index = 0;
 
-        for line in parsed_logic_board.iter() {
+        for line in self.parsed_logic_board.as_ref().unwrap().iter() {
             diagonal_in_line.push(line.get(incremented_index).unwrap().clone());
             incremented_index += 1;
         }
@@ -125,7 +117,7 @@ impl Board {
 
         diagonal_in_line.clear();
         let decremented_index = 2;
-        for (i, line) in parsed_logic_board.iter().enumerate() {
+        for (i, line) in self.parsed_logic_board.as_ref().unwrap().iter().enumerate() {
             diagonal_in_line.push(line.get(decremented_index - i).unwrap().clone());
         }
 
@@ -148,13 +140,13 @@ impl Board {
         true
     }
 
-    fn parse_logic_board(&self) -> Vec<Vec<char>> {
+    fn update_parsed_logic_board(&mut self) {
         let mut parsed_logic_board = vec![];
 
         self.board.chunks(3).for_each(|chunk| {
             parsed_logic_board.push(chunk.to_vec())
         });
 
-        parsed_logic_board
+        self.parsed_logic_board = Some(parsed_logic_board);
     }
 }
